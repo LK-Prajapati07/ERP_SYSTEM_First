302a637ae5ce800ee2681b9f3b9e98dda1875710
Lalit Kumar <prajapatilalitkumar263@outlook.com>
2026-06-06 14:45:01 +0530

    Delete Frontend/erp-project directory

--- Frontend/erp-project/src/Pages/Student/Home.jsx
+++
@@ -1,8 +0,0 @@
-const Home=()=>{
-    return (
-        <>
-        <h1>This is Student DashBoard</h1>
-        </>
-    )
-}
-export default Home
--- Frontend/erp-project/src/store/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import React from 'react'
-import rootReducer from './rootReducer'
-
-export const store=configureStore({
-    reducer:rootReducer
-})
